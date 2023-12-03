db690fd54f68dae4ff697c1597f9e92b1e43185a
Ilya-Klementev <klementevvv@yandex.ru>
2023-12-03 23:13:59 +0100

    спринт 13, первый вариант, до проверки ревьювера

--- controllers/cards.js
+++ controllers/cards.js
@@ -47,6 +47,9 @@ function putLike(req, res) {
     { $addToSet: { likes: req.user._id } },
     { new: true },)
     .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: "Передан несуществующий _id карточки" });
+      }
       return res.status(200).send(card);
     })
     .catch((err) => {
@@ -55,7 +58,6 @@ function putLike(req, res) {
       }
       return res.status(500).send({message: "Ошибка сервера"});
     })
-
 };
 
 function deleteLike(req, res) {
